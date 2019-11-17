e2d4d1e108b1ece418da262d9c8ff0bb10fef7c7
Daniel <danielseguraf@gmail.com>
2019-11-17 02:20:01 -0500

    test(ubd-button): unit testing component

--- src/components/button/button.spec.ts
+++ src/components/button/button.spec.ts
@@ -1,7 +1,21 @@
+import { newSpecPage } from "@stencil/core/dist/testing";
 import { Button } from './button';
 
 describe('ubd-button', () => {
-  it('builds', () => {
-    expect(new Button()).toBeTruthy();
+  it('should render a button component', async () => {
+    const page = await newSpecPage({
+      components: [Button],
+      html: `<ubd-button></ubd-button>`
+    });
+
+    expect(page.root).toEqualHtml(`
+      <ubd-button>
+       <mock:shadow-root>
+         <button type="button">
+           <slot></slot>
+         </button>
+       </mock:shadow-root>
+     </ubd-button>
+    `)
   });
 });
